c9a5511f9aab3842723d740eeb3be7ea0405091b
ARAFAT HOSEN EMON <arafat.emon.official@gmail.com>
2026-07-23 01:13:47 +0600

    Optimize solution using slow and fast pointers

--- Linked List/Middle_of_the_Linked_List.cpp
+++ Linked List/Middle_of_the_Linked_List.cpp
@@ -22,3 +22,19 @@ public:
        return temp;
     }
 };
+
+
+
+/*
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }return slow;
+    }
+};
+*/
